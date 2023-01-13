34523944dd300ef96c687338930b0852121dde6d
ardillahsetiawan <ardisetiawan1305@gmail.com>
2023-01-13 17:05:03 +0800

    post

---
+++ router/booksdb.js
@@ -0,0 +1,26 @@
+let books = {
+  1: {
+    author: "Ardillah Setiawan",
+    title: "BRIEF ANSWER TO THE BIG QUESTIONS",
+    reviews: {},
+  },
+  2: { author: "Colleen Hoover", title: "IT STARTS WITH US", reviews: {} },
+  3: { author: "Bonnie Garmus", title: "LESSONS IN CHEMISTRY", reviews: {} },
+  4: { author: "Danielle Steel", title: "WITHOUT A TRACE", reviews: {} },
+  5: { author: "Michelle Obama", title: "THE LIGHT WE CARRY", reviews: {} },
+  6: { author: "Jennette McCurdy", title: "I'M GLAD MY MOM DIED", reviews: {} },
+  7: { author: "Ana Reyes", title: "THE HOUSE IN THE PINES", reviews: {} },
+  8: { author: "John", title: "THE BOYS FROM BILOXI", reviews: {} },
+  9: {
+    author: "Barbara Kingsolver",
+    title: "DEMON COPPERHEAD",
+    reviews: { "Ardillah Setiawan": "So GOOD !", Daniel: "So fun to read!" },
+  },
+  10: {
+    author: "Mark Manson",
+    title: "THE SUBTLE ART OF NOT GIVING A F*CK",
+    reviews: {},
+  },
+};
+
+module.exports = books;
